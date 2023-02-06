1f2f9fd0bedf0eec3b5a4ce4cf5180baabafa306
YunSef <[sebblonski77@gmail.com](mailto:sebblonski77@gmail.com)>
2023-02-06 20:27:20 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NasaService } from './nasa.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'nasaApp';
+
+  imgOfTheDay :string = '';
+
+  constructor(public nasaService : NasaService){}
+
+  ngOnInit(): void {
+    this.nasaService.getImageOfTheDay().subscribe(data =>{
+      this.imgOfTheDay = data.url
+    })
+  }
 }
